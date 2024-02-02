764369619eaddfebdb5e9e0a15deb16a1b1add2e
jessi <jessi.nowski@hotmail.de>
2024-02-02 12:43:15 +0100

    Initial commit

---
+++ src/main/java/com/github/Main.java
@@ -0,0 +1,42 @@
+package com.github;
+
+public class Main {
+    public static void main(String[] args) {
+        //Step 2: Create a Java List for elements of type "Student" and add multiple students.
+//      List<Student> students = new ArrayList<>();
+        Student jessica = new Student("Jessica", "Zarnowski", 25678);
+        Student hanne = new Student("Hanne", "Lore", 25689);
+        Student bernd = new Student("Bernd", "Brot", 25624);
+//      students.add(jessica);
+//      students.add(hanne);
+//      students.add(bernd);
+
+        //Step 3: Create a class "School" to which students can be added through a method ('addStudent').
+        School school = new School();
+        school.addStudent(jessica);
+        school.addStudent(hanne);
+        school.addStudent(bernd);
+
+        //Step 4: Implement a method to print all students of the school (as text, to the console).
+        System.out.println(school);
+
+        //Step 5: Add a method to find a student by their ID. The found student should be returned.
+        System.out.println();
+        System.out.println(school.findStudent(25678));
+        System.out.println(school.findStudent(25677));
+
+        //Step 6: Create a method to remove a student from the school.
+        System.out.println();
+        school.removeStudent(hanne);
+        System.out.println(school);
+
+        //Bonus Course
+        System.out.println();
+        Course math = new Course("Mathe","Mueller", 3);
+        jessica.addCourseToStudent(math);
+        System.out.println(jessica);
+        System.out.println();
+        System.out.println(school.retrieveCourses(25678));
+        System.out.println(jessica.getCourseList());
+    }
+}
---
+++ src/main/java/com/github/School.java
@@ -0,0 +1,41 @@
+package com.github;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class School {
+    //Step 3: Create a class "School" to which students can be added through a method ('addStudent').
+    private List<Student> studentList = new ArrayList<>();
+
+    public void addStudent(Student student) {
+        studentList.add(student);
+    }
+
+    //Step 5: Add a method to find a student by their ID. The found student should be returned.
+    public Student findStudent(int id) {
+        for (Student students : studentList) {
+            if (students.getStudentID() == id) {
+                return students;
+            }
+        }
+        return null;
+    }
+
+    //Step 6: Create a method to remove a student from the school.
+    public void removeStudent(Student student){
+        studentList.remove(student);
+    }
+
+    //Bonus Course: Implement a method within the School class to retrieve all courses of a specific student (based on a student ID).
+    public List<Course> retrieveCourses(int studentID){
+        return findStudent(studentID).getCourseList();
+    }
+
+    //Step 4: Implement a method to print all students of the school (as text, to the console).
+    @Override
+    public String toString() {
+        return "School{" +
+                "students=" + studentList +
+                '}';
+    }
+}
